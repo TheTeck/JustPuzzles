db6b80e53cda213c1e347dd74a3b360ec602e31f
John Teckemeyer <johnteckemeyer@gmail.com>
2021-05-30 10:00:58 -0700

    Add all fields, options and handlers for input for new puzzle

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { Button, Form, Grid, Segment } from 'semantic-ui-react'
 import './AdminPage.css'
@@ -7,23 +7,135 @@ export default function AdminPage ({ admin, handleLogout }) {
     
     const [selectedFile, setSelectedFile] = useState('')
     const [state, setState] = useState({
-        caption: ''
+        width: '',
+        height: '',
+        month: '1',
+        day: '1',
+        year: '2021'
     })      
     
     const history = useHistory()
+
+    const monthOptions = [
+        { text: 'January', value: '1' },
+        { text: 'February', value: '2' },
+        { text: 'March', value: '3' },
+        { text: 'April', value: '4' },
+        { text: 'May', value: '5' },
+        { text: 'June', value: '6' },
+        { text: 'July', value: '7' },
+        { text: 'August', value: '8' },
+        { text: 'September', value: '9' },
+        { text: 'October', value: '10' },
+        { text: 'November', value: '11' },
+        { text: 'December', value: '12' },
+    ]
+
+    const dayOptions = [
+        { text: '1', value: '1' },
+        { text: '2', value: '2' },
+        { text: '3', value: '3' },
+        { text: '4', value: '4' },
+        { text: '5', value: '5' },
+        { text: '6', value: '6' },
+        { text: '7', value: '7' },
+        { text: '8', value: '8' },
+        { text: '9', value: '9' },
+        { text: '10', value: '10' },
+        { text: '11', value: '11' },
+        { text: '12', value: '12' },
+        { text: '13', value: '13' },
+        { text: '14', value: '14' },
+        { text: '15', value: '15' },
+        { text: '16', value: '16' },
+        { text: '17', value: '17' },
+        { text: '18', value: '18' },
+        { text: '19', value: '19' },
+        { text: '20', value: '20' },
+        { text: '21', value: '21' },
+        { text: '22', value: '22' },
+        { text: '23', value: '23' },
+        { text: '24', value: '24' },
+        { text: '25', value: '25' },
+        { text: '26', value: '26' },
+        { text: '27', value: '27' },
+        { text: '28', value: '28' },
+        { text: '29', value: '29' },
+        { text: '30', value: '30' },
+        { text: '31', value: '31' },
+    ]
+
+    const yearOptions = [
+        { text: '2021', value: '2021' },
+        { text: '2022', value: '2022' },
+        { text: '2023', value: '2023' },
+        { text: '2024', value: '2024' },
+        { text: '2025', value: '2025' },
+        { text: '2026', value: '2026' },
+        { text: '2027', value: '2027' },
+        { text: '2028', value: '2028' },
+        { text: '2029', value: '2029' },
+        { text: '2030', value: '2030' },
+    ]
+
 
     function handleLogoutClick () {
         handleLogout()
         history.push('/')
     }
 
-    
+    function handleFileInput(e){
+        setSelectedFile(e.target.files[0])
+    }
 
-    async function handleAddPost(post){
-        console.log('hanlde add Post')
+    function handleChange(e){
+        setState({
+          ...state,
+          [e.target.name]: e.target.value
+        })
+    }
+
+    function handleMonthChange(e, {value}) {
+        setState({
+            ...state,
+            month: value
+        })
+    }
+
+    function handleDayChange(e, {value}) {
+        setState({
+            ...state,
+            day: value
+        })
+    }
+
+    function handleYearChange(e, {value}) {
+        setState({
+            ...state,
+            year: value
+        })
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log('is handlesUbmit being called?')
+    
+        // Why do we need to create FormData
+        // what type of request are we making?
+        const formData = new FormData()
+        formData.append('photo', selectedFile)
+        formData.append('width', state.width)
+        formData.append('height', state.height)
+        
+        // Have to submit the form now! We need a function!
+        handleAddPuzzle(formData)
+    }
+
+    async function handleAddPuzzle(post){
+        console.log('handle add Post')
         try {
             
-            const data = await postService.create(post)
+            //const data = await postService.create(post)
     
         } catch(err){
             console.log(err)
@@ -31,39 +143,84 @@ export default function AdminPage ({ admin, handleLogout }) {
     }
 
     return (
-        <>
+        <div id="admin-container">
             <div onClick={handleLogoutClick}>Log Out</div>
-            <Grid textAlign='center' style={{ height: '25vh' }} verticalAlign='middle'>
+            <Grid id="form-container" textAlign='left' verticalAlign='middle'>
                 <Grid.Column style={{ maxWidth: 450 }}>
                     <Segment>
                     
                         <Form  autoComplete="off" onSubmit={handleSubmit}>
-                        
-                        <Form.Input
-                            className="form-control"
-                            type="file"
-                            name="photo"
-                            placeholder="upload image"
-                            onChange={handleFileInput}
-                        />   
-                        <Form.Input
-                            className="form-control"
-                            name="caption"
-                            value={state.caption}
-                            placeholder="What's new"
-                            onChange={handleChange}
-                            required
-                        />   
-                        <Button
-                            type="submit"
-                            className="btn"
-                        >
-                            Submit
-                        </Button>
+                            <Form.Field>
+                                <label>Puzzle Image</label>
+                                <Form.Input
+                                    className="form-control"
+                                    type="file"
+                                    name="photo"
+                                    placeholder="Upload Image"
+                                    onChange={handleFileInput}
+                                />
+                            </Form.Field>
+                            <Form.Group widths="equal">
+                                <Form.Field>
+                                    <label>Image Width (px)</label>
+                                    <Form.Input
+                                        className="form-control"
+                                        name="width"
+                                        value={state.caption}
+                                        placeholder="Width"
+                                        onChange={handleChange}
+                                        required
+                                    /> 
+                                </Form.Field> 
+                                <Form.Field>
+                                    <label>Image Height (px)</label>
+                                    <Form.Input
+                                        className="form-control"
+                                        name="height"
+                                        value={state.caption}
+                                        placeholder="Height"
+                                        onChange={handleChange}
+                                        required
+                                    />  
+                                </Form.Field>
+                            </Form.Group>
+                                <Form.Select  
+                                    label="Month"          
+                                    name="month"
+                                    defaultValue="1"
+                                    options={monthOptions}
+                                    onChange={handleMonthChange}
+                                    required
+                                />
+                                <Form.Select    
+                                    label="Day"          
+                                    name="day"
+                                    defaultValue="1"
+                                    options={dayOptions}
+                                    onChange={handleDayChange}
+                                    required
+                                />
+                                <Form.Select  
+                                    label="Year"          
+                                    name="year"
+                                    defaultValue="2021"
+                                    options={yearOptions}
+                                    onChange={handleYearChange}
+                                    required
+                                />
+                            <div style={{ textAlign: "center" }}>
+                                <Button
+                                    type="submit"
+                                    className="btn"
+                                    color="red"
+                                >
+                                    Submit
+                                </Button>
+                            </div>
                         </Form>
                     </Segment>
                 </Grid.Column>
             </Grid>
-        </>
+        </div>
     )
 }
