b43311c6d40961221b110877ca1e8426e89a4a29
John Teckemeyer <johnteckemeyer@gmail.com>
2021-05-29 23:50:12 -0700

    Add form for new image for puzzle

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import { Button, Form, Grid, Segment } from 'semantic-ui-react'
 import './AdminPage.css'
 
 export default function AdminPage ({ admin, handleLogout }) {
-
+    
+    const [selectedFile, setSelectedFile] = useState('')
+    const [state, setState] = useState({
+        caption: ''
+    })      
+    
     const history = useHistory()
 
     function handleLogoutClick () {
@@ -11,10 +17,53 @@ export default function AdminPage ({ admin, handleLogout }) {
         history.push('/')
     }
 
+    
+
+    async function handleAddPost(post){
+        console.log('hanlde add Post')
+        try {
+            
+            const data = await postService.create(post)
+    
+        } catch(err){
+            console.log(err)
+        }
+    }
+
     return (
-        <div>
+        <>
             <div onClick={handleLogoutClick}>Log Out</div>
-            This is the admin page
-        </div>
+            <Grid textAlign='center' style={{ height: '25vh' }} verticalAlign='middle'>
+                <Grid.Column style={{ maxWidth: 450 }}>
+                    <Segment>
+                    
+                        <Form  autoComplete="off" onSubmit={handleSubmit}>
+                        
+                        <Form.Input
+                            className="form-control"
+                            type="file"
+                            name="photo"
+                            placeholder="upload image"
+                            onChange={handleFileInput}
+                        />   
+                        <Form.Input
+                            className="form-control"
+                            name="caption"
+                            value={state.caption}
+                            placeholder="What's new"
+                            onChange={handleChange}
+                            required
+                        />   
+                        <Button
+                            type="submit"
+                            className="btn"
+                        >
+                            Submit
+                        </Button>
+                        </Form>
+                    </Segment>
+                </Grid.Column>
+            </Grid>
+        </>
     )
 }
