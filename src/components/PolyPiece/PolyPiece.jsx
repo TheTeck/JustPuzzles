e2d5016037146415b4a33b8fee6ea3a5932891cc
John Teckemeyer <johnteckemeyer@gmail.com>
2021-06-05 19:27:49 -0700

    Update TypeSelectPage to only show proper playable counts

--- src/components/PolyPiece/PolyPiece.jsx
+++ src/components/PolyPiece/PolyPiece.jsx
@@ -11,6 +11,7 @@ export default function PolyPiece ({ image, size, id, piece, vEdges, hEdges, xCo
     const [xOffset, setXOffset] = useState()
     const [yOffset, setYOffset] = useState()
     const [edges, setEdges] = useState([[0,0],[0,0],[0,0],[0,0]])
+    const [perimeter, setPerimeter] = useState([15,85,85,15])
 
     function setTheWidth () {
         if (/left/.test(piece.description) || /right/.test(piece.description))
@@ -40,21 +41,31 @@ export default function PolyPiece ({ image, size, id, piece, vEdges, hEdges, xCo
         temp.push(piece.y < yCount-1 ? [hEdges[piece.y][piece.x][1], hEdges[piece.y][piece.x][0]] : [50,0])
         temp.push(piece.x ? [vEdges[piece.y][piece.x-1][0], vEdges[piece.y][piece.x-1][1]] : [0,50])
 
-        // temp.push(piece.y ? [hEdges[piece.y-1][piece.x][0], hEdges[piece.y-1][piece.x][1]] : null)
-        // temp.push(piece.x < xCount-1 ? [vEdges[piece.y][piece.x][0], vEdges[piece.y][piece.x][1]] : null)
-        // temp.push(piece.y < yCount-1 ? [hEdges[piece.y][piece.x][0], hEdges[piece.y][piece.x][1]] : null)
-        // temp.push(piece.x ? [vEdges[piece.y][piece.x-1][0], vEdges[piece.y][piece.x-1][1]] : null)
         setEdges(temp)
     }
 
+    function setThePerimeter () {
+        const temp = [...perimeter]
+
+        if (/top/.test(piece.description))
+            temp[0] = 0
+        if (/right/.test(piece.description))
+            temp[1] = 100
+        if (/bottom/.test(piece.description))
+            temp[2] = 100
+        if (/left/.test(piece.description)) 
+            temp[3] = 0
+        
+        setPerimeter(temp)
+    }
     useEffect(() => {
         setTheWidth()
         setTheHeight()
         setTheOffset()
         setTheEdges()
+        setThePerimeter()
     }, [])
 
-    console.log(edges)
     return (
         <div 
             id={id}
@@ -69,14 +80,12 @@ export default function PolyPiece ({ image, size, id, piece, vEdges, hEdges, xCo
                 zIndex: `${piece.z}`,
                 fontSize: '20px',
                 color: 'white',
-                // clipPath: `polygon(15% 15%, 15% 85%, 85% 85%, 85% 15%)`,
-                clipPath: `polygon(15% 15%, ${edges[0][0]}% ${edges[0][1]+15}%, 85% 15%, ${edges[1][0]+85}% ${edges[1][1]}%, 85% 85%, ${edges[2][0]}% ${edges[2][1]+85}%, 15% 85%, ${edges[3][0]+15}% ${edges[3][1]}%)`
-
+                clipPath: `polygon(${perimeter[3]}% ${perimeter[0]}%, ${edges[0][0]}% ${edges[0][1]+perimeter[0]}%, 
+                                    ${perimeter[1]}% ${perimeter[0]}%, ${edges[1][0]+perimeter[1]}% ${edges[1][1]}%, 
+                                    ${perimeter[1]}% ${perimeter[2]}%, ${edges[2][0]}% ${edges[2][1]+perimeter[2]}%, 
+                                    ${perimeter[3]}% ${perimeter[2]}%, ${edges[3][0]+perimeter[3]}% ${edges[3][1]}%)`
             }}
-            >
-                <div className="red-box" style={{ width: size, height: size, left: xOffset, top: yOffset }}>
-                    {piece.x}, {piece.y}
-                </div>
-            </div>
+        >
+        </div>
     )
 } 
