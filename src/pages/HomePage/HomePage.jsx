b88ca88369d7119b50e9c8bc901172896d0d8a27
John Teckemeyer <johnteckemeyer@gmail.com>
2021-06-06 11:22:26 -0700

    Add buttons for pagination and refactor puzzle state to only call database once and not every time page changes

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,9 +7,10 @@ import './HomePage.css'
 
 export default function HomePage ({ pageNum }) {
 
-    const [puzzles, setPuzzles] = useState([])
-    const [page, setPage] = useState(pageNum)
-    const [totalPages, setTotalPages] = useState(0)
+    const [ allPuzzles, setAllPuzzles ] = useState([])
+    const [ pagePuzzles, setPagePuzzles ] = useState([])
+    const [ page, setPage ] = useState(pageNum)
+    const [ totalPages, setTotalPages ] = useState(0)
 
     const history = useHistory()
 
@@ -17,12 +18,28 @@ export default function HomePage ({ pageNum }) {
         history.push(`/puzzle/${id}/options`)
     }
 
-    async function getPuzzles(pageNum) {
+    function handleLeftClick () {
+        if (page > 1) {
+            setPage(page - 1)
+        }
+    }
+
+    function handleRightClick () {
+        if (page < totalPages) {
+            setPage(page + 1)
+        }
+    }
+
+    function updatePuzzleList () {
+        const onePageOfPuzzles = allPuzzles.slice(10*(page-1), 10*page)
+        setPagePuzzles(onePageOfPuzzles)
+    }
+
+    async function getPuzzles () {
         try {
             const data = await puzzleService.getAll();
             data.puzzles = data.puzzles.reverse()
-            const onePageOfPuzzles = data.puzzles.slice(10*(page-1), 10*page)
-            setPuzzles([...onePageOfPuzzles])
+            setAllPuzzles(data.puzzles)
             setTotalPages(Math.ceil(data.puzzles.length / 10))
         } catch(err){
             console.log(err, 'Could not load puzzles')
@@ -30,14 +47,22 @@ export default function HomePage ({ pageNum }) {
     }
 
     useEffect(() => {
-        getPuzzles(page)
+        updatePuzzleList()
+    })
+
+    useEffect(() => {
+        getPuzzles()
     }, [])
 
     return (
         <div id="home-container">
             <Header />
-            <div>{page} of {totalPages} Pages</div>
-            <Feed puzzles={puzzles} showPuzzle={showPuzzle} />
+            <div className="pagination-display">
+                <div onClick={handleLeftClick} className="left-arrow"></div>
+                {page} of {totalPages} Pages
+                <div onClick={handleRightClick} className="right-arrow"></div>
+            </div>
+            <Feed puzzles={pagePuzzles} showPuzzle={showPuzzle} />
         </div>
     )
 }
