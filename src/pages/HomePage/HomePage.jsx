6e52e69aa563df1aea1d99cdba2b13e796a8b9e2
John Teckemeyer <johnteckemeyer@gmail.com>
2021-06-05 23:03:43 -0700

    Display available puzzle in chronological order from newest to oldest and add pagination display

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,6 +9,7 @@ export default function HomePage ({ pageNum }) {
 
     const [puzzles, setPuzzles] = useState([])
     const [page, setPage] = useState(pageNum)
+    const [totalPages, setTotalPages] = useState(0)
 
     const history = useHistory()
 
@@ -19,8 +20,10 @@ export default function HomePage ({ pageNum }) {
     async function getPuzzles(pageNum) {
         try {
             const data = await puzzleService.getAll();
+            data.puzzles = data.puzzles.reverse()
             const onePageOfPuzzles = data.puzzles.slice(10*(page-1), 10*page)
             setPuzzles([...onePageOfPuzzles])
+            setTotalPages(Math.ceil(data.puzzles.length / 10))
         } catch(err){
             console.log(err, 'Could not load puzzles')
         }
@@ -33,6 +36,7 @@ export default function HomePage ({ pageNum }) {
     return (
         <div id="home-container">
             <Header />
+            <div>{page} of {totalPages} Pages</div>
             <Feed puzzles={puzzles} showPuzzle={showPuzzle} />
         </div>
     )
